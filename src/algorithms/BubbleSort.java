fc38e0dc99769536346e7ef85aca821f0ef573ca
oscarneiva <oscarneivaeneto@gmail.com>
2019-10-07 15:01:28 -0300

    Initial commit

---
+++ src/algorithms/BubbleSort.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ *
+ * @author Oscar Neiva
+ */
+public class BubbleSort {
+    private int[][] array;
+    private LinkedList<Integer> linkedlist;
+    private Queue<Integer> queue;
+    private Stack<String> stackOfCards;
+    
+    public BubbleSort(int x, int y){
+        array = new int[x][y];
+        linkedlist = new LinkedList<Integer>();
+        queue = new LinkedList<Integer>();
+        stackOfCards = new Stack<>();
+    } 
+    
+    public void readTextFile() throws IOException{
+        FileReader fileReader = new FileReader("./data/data.csv");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        
+        String fileContent; 
+        String[] fileData = null;
+        while((fileContent = bufferedReader.readLine()) != null) {
+            fileContent.trim();
+            fileData = fileContent.split(","); 
+            
+            //user = new User(fileData[0], fileData[1], fileData[2]);
+            //users.add(user);
+        }
+         
+        for(int i = 0; i < 10000; i++) {
+            System.out.println(fileData[i]);
+        }
+        
+        fileReader.close();
+    }
+}
---
+++ src/sortingalgorithms/SortingAlgorithms.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sortingalgorithms;
+
+import algorithms.BubbleSort;
+import java.io.IOException;
+
+/**
+ *
+ * @author Oscar Neiva
+ */
+public class SortingAlgorithms {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        BubbleSort bubble = new BubbleSort(10,10);
+        bubble.readTextFile();
+    }
+    
+}
